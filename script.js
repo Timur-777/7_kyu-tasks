089f573aab57a6d5b68218d997e3c5ad80eb509c
Ruslan <lechi002@inbox.ru>
2022-08-29 14:01:00 +0400

    rentalCarCost

--- script.js
+++ script.js
@@ -154,3 +154,13 @@ function sumMul(n, m) {
   }
   return res;
 }
+
+function rentalCarCost(d) {
+  if (d >= 3 && d < 7) {
+    return d * 40 - 20;
+  }
+  if (d >= 7) {
+    return d * 40 - 50;
+  }
+  return d * 40;
+}
