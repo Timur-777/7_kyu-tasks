20e68a5c8de5339f1ac2a4f330159a813d904a37
Ruslan <lechi002@inbox.ru>
2022-09-06 12:34:04 +0400

    countBy

--- script.js
+++ script.js
@@ -222,3 +222,11 @@ function toAcronym(inp) {
     .join("")
     .toUpperCase();
 }
+
+function countBy(x, n) {
+  let z = [];
+  for (i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+  return z;
+}
