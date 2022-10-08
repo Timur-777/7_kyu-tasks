c6592b6d2f32325ffce1488784a1e4bb84744467
Ruslan <lechi002@inbox.ru>
2022-10-08 12:06:47 +0400

    spinWords

--- script.js
+++ script.js
@@ -413,3 +413,12 @@ function numberJoy(n) {
   let sum2 = parseInt(sum.toString().split("").reverse().join(""));
   return n === sum * sum2;
 }
+
+function spinWords(string) {
+  return string
+    .split(" ")
+    .map((item) =>
+      item.length >= 5 ? item.split("").reverse().join("") : item
+    )
+    .join(" ");
+}
